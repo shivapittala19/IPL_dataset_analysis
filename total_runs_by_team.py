d341744cb4f9ea1b6907f42825e9f548bbc5cc9f
shivapittala19 <shivapittala2001@gmail.com>
2023-10-21 22:00:14 +0530

    pylint with file1

--- total_runs_by_team.py
+++ total_runs_by_team.py
@@ -1,20 +1,22 @@
+"""
+This Module is to find the total runs scored by team
+"""
 import matplotlib.pyplot as plt
 from main import deliveries,color_codes
 
 def total_runs_scored_by_team():
+    """ runs scored by each team icombined all seasons """
     runs_dict = {}
     #creating a dictinary with key as a team and runs scored as it's value
     for row in deliveries:
         batting_team = row['batting_team']
         runs = int(row['total_runs'])
-        try:
+        if batting_team in runs_dict:
             runs_dict[batting_team] += runs
-        except:
+        else:
             runs_dict[batting_team] = runs
-            
     x_values = list(runs_dict.keys())
     y_values = list(runs_dict.values())
-    
     plt.bar(x_values,y_values,color = color_codes,label = x_values,width=0.5)
     plt.xlabel("Team")
     plt.ylabel("Runs scored by team")
