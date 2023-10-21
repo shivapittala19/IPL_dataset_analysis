a31c9b3d9fe227ed5a02c5bde2cdf66fba76bd3a
shivapittala19 <shivapittala2001@gmail.com>
2023-10-21 22:05:16 +0530

    structuring based on pylint

--- top_ten_rcb_batsman.py
+++ top_ten_rcb_batsman.py
@@ -1,24 +1,22 @@
+""" Find top rcb batsmans"""
 import matplotlib.pyplot as plt
 from main import deliveries
 
 def top_ten_rcb_bastman():
+    """ top 10 rcb batsmans """
     batsman_runs = {}
-    
     # creating a dictinory with batsman as key and runs as it's value
     for row in deliveries:
         if row['batting_team'] == 'Royal Challengers Bangalore':
             batter, runs = row['batsman'] , int(row['batsman_runs'])
             try:
                 batsman_runs[batter] += runs
-            except:
+            except KeyError:
                 batsman_runs[batter] = runs
-    
     #sorting the batsman runs to get top 10 batsman
     sorting_order = dict(sorted(batsman_runs.items(),key=lambda x: x[1],reverse=True))
-    
     batsman = list(sorting_order.keys())[:10]
     batsman_runs = list(sorting_order.values())[:10]
-    
     plt.bar(batsman,batsman_runs)
     plt.xlabel("Batsman")
     plt.ylabel("Runs")
